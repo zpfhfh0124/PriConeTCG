8800e5b06486f426741caf14deae9d14e2ef3a9c
zpfhfh0124 <48612497+zpfhfh0124@users.noreply.github.com>
2024-08-10 19:20:14 +0900

    카드 프리팹 작업
    카드 프리팹에 card 스크립트 컴포넌트 추가
    컴포넌트 필드에 각 오브젝트 연결

--- Assets/Scripts/Card/Card.cs
+++ Assets/Scripts/Card/Card.cs
@@ -7,14 +7,34 @@ public class Card : MonoBehaviour
 {
     [SerializeField] SpriteRenderer _card_background;
     [SerializeField] SpriteRenderer _character;
-    [SerializeField] TextMeshProUGUI _text_name;
-    [SerializeField] TextMeshProUGUI _text_atk;
-    [SerializeField] TextMeshProUGUI _text_hp;
-    [SerializeField] TextMeshProUGUI _card_front;
-    [SerializeField] TextMeshProUGUI _card_back;
+    [SerializeField] TextMeshPro _text_name;
+    [SerializeField] TextMeshPro _text_atk;
+    [SerializeField] TextMeshPro _text_hp;
+    [SerializeField] Sprite _card_front;
+    [SerializeField] Sprite _card_back;
 
-    public Item item;
-    bool isFront;
+    public Item _item;
+    bool _isFront;
 
+    public void Setup(Item item, bool isFront)
+    {
+        _item = item;
+        _isFront = isFront;
 
+        // 앞면일 경우
+        if (isFront)
+        {
+            _character.sprite = item.sprite;
+            _text_name.text = item.name;
+            _text_atk.text = item.ap.ToString();
+            _text_hp.text = item.hp.ToString();
+        }
+        else
+        {
+            _card_background.sprite = _card_back;
+            _text_name.text = string.Empty;
+            _text_atk.text = string.Empty;
+            _text_hp.text = string.Empty;
+        }
+    }
 }
